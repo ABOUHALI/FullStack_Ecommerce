304ce28310c2a7a985eac1f8ab1fe827b0bf0113
ABOUHALI <abouhaliayman3@gmail.com>
2024-01-28 19:16:59 +0100

    file home component jsx updated

--- e-commerce/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ e-commerce/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Button } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const HomeSectionCarousel = () => {
-  return (
-    <div>HomeSectionCarousel</div>
-  )
-}
+    const [activeIndex,setActiveIndex]=useState(0);
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 5.5 },
+  };
+  const slidePrev=()=>setActiveIndex(activeIndex-1)
+  const slideNext=()=>setActiveIndex(activeIndex+1)
 
-export default HomeSectionCarousel
+  const syncActiveIndex=({item})=>setActiveIndex(item)
+
+  const items = [1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  return (
+    <div className="border">
+      <div className="relative p-5 ">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        <Button
+          variant="contained"
+          className="z-50 bg-white"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <ArrowBackIosNewIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>
+
+        <Button
+          variant="contained"
+          className="z-50 bg-white"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "-4rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <ArrowBackIosNewIcon
+            sx={{ transform: "rotate(-90deg)", color: "black" }}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarousel;
--- e-commerce/src/customer/components/Pages/HomePage/HomePage.jsx
+++ e-commerce/src/customer/components/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import MainCarousel from '../../HomeCarousel/MainCarousel'
-import HomeSectionCard from '../../HomeSectionCard/HomeSectionCard'
+import HomeSectionCarousel from '../../HomeSectionCarousel/HomeSectionCarousel'
 
 function HomePage() {
   return (
     <div>
         <MainCarousel/>
-        <div>
-            <HomeSectionCard/>
+        <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+            <HomeSectionCarousel/>
+            <HomeSectionCarousel/>
+            <HomeSectionCarousel/>
+            <HomeSectionCarousel/>
+
         </div>
     </div>
   )
