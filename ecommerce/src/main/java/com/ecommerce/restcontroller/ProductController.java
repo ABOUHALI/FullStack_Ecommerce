5f31fbc718a9feec1aed9b31f8101cd907bcca0c
ABOUHALI <abouhaliayman3@gmail.com>
2024-02-19 14:22:08 +0100

    adding product admin controller

---
+++ ecommerce/src/main/java/com/ecommerce/restcontroller/ProductController.java
@@ -0,0 +1,64 @@
+package com.ecommerce.restcontroller;
+
+
+import com.ecommerce.exception.ProductException;
+import com.ecommerce.model.Product;
+import com.ecommerce.request.ProductReq;
+import com.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductReq product) throws ProductException {
+        Product newProduct = productService.createProduct(product);
+        return new ResponseEntity<Product>(newProduct, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long productId) throws ProductException {
+        String deleteAlert = productService.deleteProduct(productId);
+
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+
+    public HttpEntity<?> addProducts(@RequestBody ProductReq[] products) throws ProductException {
+        for (ProductReq product: products) {
+            productService.createProduct(product);
+        }
+        return ResponseEntity.ok(products);
+
+    }
+
+
+    @PutMapping("/update/{productId}")
+    public HttpEntity<?> updateProduct(@PathVariable Long productId,@RequestBody Product product ) throws ProductException {
+            Product productUpd = productService.updateProduct(productId,product);
+            return ResponseEntity.ok(productUpd);
+
+    }
+
+
+
+
+
+
+
+}
--- ecommerce/src/main/java/com/ecommerce/service/impl/OrderItemServiceImpl.java
+++ ecommerce/src/main/java/com/ecommerce/service/impl/OrderItemServiceImpl.java
@@ -1,14 +1,22 @@
 package com.ecommerce.service.impl;
 
 import com.ecommerce.model.OrderItem;
+import com.ecommerce.repository.OrderItemRepository;
 import com.ecommerce.service.OrderItemService;
 import com.ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderItemServiceImpl implements OrderItemService {
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+
     @Override
     public OrderItem createOrderItem(OrderItem orderItem) {
-        return null;
+
+        return orderItemRepository.save(orderItem);
     }
 }
